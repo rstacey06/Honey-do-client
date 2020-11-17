07fc4d4e8e4f7cfdbdf36b494889f2bf57b6b39d
github username <github email address>
2020-11-17 10:10:49 -0500

    create store and import needed libraries

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import rootReducer from './reducers/index';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||
 compose;
 
 const store = createStore(rootReducer,composeEnhancers(applyMiddleware(thunk)));
 
-
-ReactDOM.render( <App />, document.getElementById('root'));
-
+ReactDOM.render((
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>),
+document.getElementById('root')
+);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
