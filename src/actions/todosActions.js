8a4f218e42967a81f2eaa6f934ce712269bae70a
github username <github email address>
2020-11-17 12:34:39 -0500

    create fetch goal by id

--- src/actions/todosActions.js
+++ src/actions/todosActions.js
@@ -1,10 +1,20 @@
 const todosURL = "http://localhost:3000/api/todos"
 
 export const fetchTodos = () => {
-    return (dispatch) => {
-        dispatch({type: 'LOADING_TODOS'})
-            return fetch(todosURL)
-                .then(response => response.json())
-                .then(todos => dispatch({type: 'FETCH_TODOS', payload: todos}))
-              }
-            }
+  return (dispatch) => {
+    dispatch({type: 'LOADING_TODOS'})
+      return fetch(todosURL)
+      .then(response => response.json())
+      .then(todos => dispatch({type: 'FETCH_TODOS', payload: todos}))
+    }
+  }
+
+// fetch a particular Todo by id
+export const fetchTodo = id => {
+  return (dispatch) => {
+    dispatch({type: 'LOADING_TODOS'})
+      return fetch(`${todosURL}/${id}`)
+      .then(response => response.json())
+      .then(todo => dispatch({type: 'FETCH_TODO', payload: todo}))
+    }
+  }
