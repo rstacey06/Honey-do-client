78fc34910d5a9321996a372795d3dda948c42734
github username <github email address>
2020-11-17 12:45:16 -0500

    add template for homepage and routes

---
+++ src/containers/Main.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+import { Route } from 'react-router-dom';
+//import Home from '../components/Home';
+//import TodoInput from '../components/Todos/TodoInput';
+import TodosContainer from './TodosContainer';
+import GoalsContainer from './GoalsContainer';
+//import MainHeader from '../components/MainHeader';
+//import { Container } from 'semantic-ui-react';
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <MainHeader />
+        <Container>
+          <NavBar />
+
+         <Container>
+           <Route />
+           <Route />
+           <Route />
+           <Route />
+         </Container>
+       </Container>
+    </div>
+        )
+    }
+}
+export default Main;
