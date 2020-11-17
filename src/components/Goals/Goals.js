ca31c60a20e9cae824ba597a522f8ab6488a785f
github username <github email address>
2020-11-17 13:08:31 -0500

    checking render

---
+++ src/components/Goals/Goals.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { addTodo } from '../../actions/todosActions';
+import { connect } from 'react-redux';
+
+class TodoInput extends Component {
+  render(){
+    return(
+      <div>
+      GoalInput
+      </div>
+    )
+  }
+}
