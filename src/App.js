5f91138cab8f4c24ff19c409a87ed8b2833e092c
github username <github email address>
2020-11-17 09:53:33 -0500

    installed react-redux, react-router-dom, redux ,redux-thunk

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 
 class App extends React.Component {
 
-  componentDidMount() {
-   fetch('http://localhost:3000/api/todos/3/goals')
-   .then(response => response.json())
-   .then(data => console.log(data))
- }
+ //  componentDidMount() {
+ //   fetch('http://localhost:3000/api/todos/3/goals')
+ //   .then(response => response.json())
+ //   .then(data => console.log(data))
+ // }
 
   render() {
     return (
