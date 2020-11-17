a966db2754236d90c48b2abdebb56eed00bef72f
github username <github email address>
2020-11-17 13:13:56 -0500

    create/return todosList

--- src/components/Goals/Goals.js
+++ src/components/Goals/Goals.js
@@ -1,13 +1,1 @@
-import React, { Component } from 'react';
-import { addTodo } from '../../actions/todosActions';
-import { connect } from 'react-redux';
 
-class TodoInput extends Component {
-  render(){
-    return(
-      <div>
-      GoalInput
-      </div>
-    )
-  }
-}
---
+++ src/components/Todos/Todos.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Todo from './Todo';
+// import { Container } from 'semantic-ui-react';
+
+const Todos = props => {
+
+    const todosList =  props.todos.map(todo =>
+        <Todo
+            key={todo.id}
+            todo={todo}
+            {...todo}
+            deleteTodo={props.deleteTodo}
+        />)
+    return (
+        <Container>
+            {todosList}
+        </Container>
+    );
+};
+export default Todos;
