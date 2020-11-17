e5dedb8a339c52df36bb3223d4dc17dbdf906b0d
github username <github email address>
2020-11-17 14:25:54 -0500

    create goals list and attr.

--- src/components/Goals/Goals.js
+++ src/components/Goals/Goals.js
@@ -1,1 +1,29 @@
+import React from 'react';
+import Goal from './Goal';
 
+const Goals = (props) => {
+    const goalsList = props.goals.map(goal =>
+        <li className="goal-items" key={goal.id}>
+            <Goal
+
+                id={goal.id}
+                completed={goal.completed}
+                description={goal.description}
+                deleteGoal={props.deleteGoal}
+                />
+        </li>)
+
+    return (
+        <div className="goals-box">
+            <h1>{props.todo.target}</h1>
+            <label><strong>PLAN</strong></label>
+            <p>{props.todo.plan}</p>
+            <strong>GOALS</strong>
+            <ul>
+                {goalsList}
+            </ul>
+        </div>
+    );
+};
+
+export default Goals;
