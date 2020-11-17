5a7583b58cb267b6b63eb27946ff6937ee181194
github username <github email address>
2020-11-17 13:45:45 -0500

    added homeheader template

---
+++ src/components/MainHeader.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// App name header on top of page
+
+const MainHeader = () => (
+    <div className="App-header">
+        <h1>HoneyDO</h1>
+    </div>
+)
+
+export default MainHeader;
