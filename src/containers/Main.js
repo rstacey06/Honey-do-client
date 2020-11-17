97f54993287fbc99331c05812dd45bf5de02cc46
github username <github email address>
2020-11-17 13:29:19 -0500

    imported files

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import NavBar from '../components/NavBar';
 import { Route } from 'react-router-dom';
-//import Home from '../components/Home';
-//import TodoInput from '../components/Todos/TodoInput';
+import Home from '../components/Home';
+import TodoInput from '../components/Todos/TodoInput';
 import TodosContainer from './TodosContainer';
 import GoalsContainer from './GoalsContainer';
-//import MainHeader from '../components/MainHeader';
+import MainHeader from '../components/MainHeader';
 //import { Container } from 'semantic-ui-react';
 
 class Main extends Component {
