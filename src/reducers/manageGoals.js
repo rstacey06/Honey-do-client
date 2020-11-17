dcaf332f1cdcd9ce40e9b245a7323a3d2e44347c
github username <github email address>
2020-11-17 14:22:32 -0500

    created reducer for goals

---
+++ src/reducers/manageGoals.js
@@ -0,0 +1,18 @@
+export default function goalsReducer(state = {
+    loading: false,
+    goalsData: []},
+    action){
+
+    switch(action.type){
+        case 'FETCH_GOALS':
+            return {...state, goalsData: action.payload.goals}
+        case 'LOADING_GOALS':
+            return {...state, loading: true}
+        case 'CREATE_OBJECTIVE':
+            return {...state, goalsData:[...state.goalsData, action.payload.goal]}
+        case 'DELETE_OBJECTIVE':
+            return {...state, loading: false, goalsData: state.goalsData.filter(goal => goal.id !== action.payload.id )}
+        default:
+            return state;
+          }
+        }
