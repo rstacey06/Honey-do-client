090e1ad4e6de3a4119e2bdc3eb4d2650d2707283
github username <github email address>
2020-11-17 14:05:53 -0500

    added Todos link and delete button

---
+++ src/components/Todos/Todo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Button } from 'semantic-ui-react';
+import { NavLink } from 'react-router-dom';
+
+// complete button deletes the todo
+
+const Todo = ({ target, deleteTodo, id }) => {
+
+    return(
+        <div className="todo">
+            <NavLink key={id} to={`/todos/${id}/goals`}><h3>{target}</h3></NavLink>
+            <Button className="delete-button" color='red' size='mini' onClick={() => deleteTodo(id)}>X</Button>
+            <Button className="complete-button" color='green' size='mini' onClick={() => deleteTodo(id)}>Done!</Button>
+        </div>
+    )
+}
+
+export default Todo;
